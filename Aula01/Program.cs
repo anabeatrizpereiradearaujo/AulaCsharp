675951e5927cf1e3f8f91a5b5c99877c63cd14be
anabeatrizpereiradearaujo <biabailarina.2006@gmail.com>
2025-02-26 14:48:57 -0300

    Upload dos códigos da primeira aula

---
+++ Aula01/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Aula01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int idade = 18;
+            string nomeCompleto = "Ana Beatriz Pereira de Araujo";
+
+            var variavelSemTipo = "Ana";
+
+            Console.WriteLine(variavelSemTipo.GetType());
+
+            Console.WriteLine("Digite seu nome:");
+
+            string nomeUsuario = Console.ReadLine();
+
+            Console.WriteLine("Digite sua idade");
+            string idadeUsuario = Console.ReadLine();
+
+
+           
+
+
+            Console.Write($" Meu nome é {nomeUsuario} e a Minha idade é {idadeUsuario}");
+        }
+
+    }
+ }
